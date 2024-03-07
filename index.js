19879c2bf1e484e0f45de34d382327eb81329e9b
Wills <wmanley@pinterest.com>
2024-03-07 13:15:56 -0600

    size

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1430,
+    height: 1030,
     webPreferences: {
       nodeIntegration: true
     }
